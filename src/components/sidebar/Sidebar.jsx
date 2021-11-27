919e0d588cab037980afe20aa857384c8115412e
trunganh05071995 <nonameex2@gmail.com>
2021-11-27 17:08:06 +0700

    send sms feature

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { NavLink } from 'react-router-dom'
 
@@ -7,6 +7,12 @@ import './sidebar.css'
 import logo from '../../assets/images/logo.png'
 
 import sidebar_items from '../../assets/JsonData/sidebar_routes.json'
+import { useDispatch } from 'react-redux'
+import { ACTIVE_KEY, FEED_BUZZER, FEED_DRV, FEED_GAS, FEED_LED, history } from '../../config/config'
+import axios from 'axios'
+import { getHistory } from '../../redux/actions/DataAction'
+import { getLatestValue, parseValue } from '../../redux/actions/AdafruitAction'
+import { getPhoneList } from '../../redux/actions/UserAction'
 
 const SidebarItem = props => {
 
@@ -25,11 +31,22 @@ const SidebarItem = props => {
 }
 
 const Sidebar = props => {
-
     const activeItem = sidebar_items.findIndex(item => item.route === props.location.pathname)
+    const dispatch = useDispatch();
+    useEffect(async () => {
+        console.log(history.location.pathname);
+        Promise.all([
+            getLatestValue(dispatch), 
+            getHistory(dispatch),
+            getPhoneList(dispatch),
 
+        ])
+        return () => {
+        }
+    }, [])
     return (
         <div className='sidebar'>
+            {console.log("Slide Bar")}
             <div className="sidebar__logo">
                 <img src={logo} alt="company logo" />
             </div>
--- src/redux/actions/UserAction.js
+++ src/redux/actions/UserAction.js
@@ -1,12 +1,89 @@
 import axios from "axios"
+import { history } from "../../config/config"
+import { checkLogin } from "./Helper"
 
-export const loginFeature = async ({ usr, pass }) => {
+export const loginFeature = ({ usr, pass }) => {
+    return async (dispatch) => {
+        console.log("login")
+        try {
+            let result = await axios({
+                method: 'POST',
+                url: 'http://localhost:7000/login',
+                data: {
+                    usr, pass
+                }
+            })
+            console.log(result.data);
+            const { token, phonelist, settings, name } = result.data;
+            localStorage.setItem('accessToken', token)
+            localStorage.setItem('name', name)
+            localStorage.setItem('settings', JSON.stringify(settings))
+            history.push('/')
+        } catch (error) {
+            // console.log(error?.response.data)   
+        }
+    }
+
+}
+export const getSetting = () => {
+    let settings = localStorage.getItem('settings')
+    return JSON.parse(settings)
+}
+export const getInfo = async () => {
+    if (!checkLogin()) return history.push('/login')
+    let token = localStorage.getItem("accessToken")
     let result = await axios({
-        method: 'POST',
-        url: 'http://localhost:7000/login',
-        data: {
-            usr, pass
+        url: 'http://localhost:7000/info',
+        method: 'get',
+        headers: {
+            authorization: token
         }
     })
-    console.log(usr, pass)
+    return result.data
+}
+export const updateInfo = async (data) => {
+    if (!checkLogin()) return history.push('/login')
+    let token = localStorage.getItem("accessToken")
+    let result = await axios({
+        url: 'http://localhost:7000/info',
+        method: 'put',
+        data,
+        headers: {
+            authorization: token
+        }
+    })
+}
+
+
+export const getPhoneList = async (dispatch) => {
+    if (!checkLogin()) return history.push('/login');
+    let token = localStorage.getItem("accessToken")
+    let result = await axios({
+        url: `http://localhost:7000/phonelist`,
+        method: "GET",
+        headers: {
+            authorization: token
+        }
+    })
+    console.log("PhoneList: ", result.data);
+    dispatch({
+        type: "PHONE_LIST",
+        data: result.data
+    });
+    return result.data
+}
+export const sendSMS = () => {
+    if (!checkLogin()) return history.push('/login')
+    let token = localStorage.getItem("accessToken")
+    axios({
+        url: 'http://localhost:7000/sms',
+        method: 'get',
+        headers: {
+            authorization: token
+        }
+    })
+}
+export const logout = () => {
+    localStorage.clear();
+    return history.push('/login')
 }
